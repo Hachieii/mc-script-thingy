e396cb868b1e614f7128660d916b9ea1818db54d
Hachieii <hachieii07@gmail.com>
2026-04-20 17:43:26 +0700

    add automation for crafting

--- tutien.py
+++ tutien.py
@@ -24,7 +24,7 @@ def is_dead(x, y, z):
     delta = sqrt((x - cx)*(x - cx) + (y - cy)*(y - cy) + (z - cz)*(z - cz))
     return delta > 1.5
 
-def main():
+def farming():
     while 1:
         go_to_farm_land()
         
@@ -34,6 +34,35 @@ def main():
         while not is_dead(x, y, z):
             time.sleep(1)
             continue
+        
+def price_parser(x):
+    base = {
+        'K': 1000,
+        'M': 1000000,
+        'B': 1000000000
+    }
     
+    last = x[-1]
+    if last not in base:
+        return float(x)
+    
+    return float(x[:len(x) - 1]) * base[last]
+
+def have_enough_khoang_thach(ammount):
+    data = utils.get_scoreboard_info()
+    cur = price_parser(data["khoang_thach"])
+    return cur >= ammount
+        
+def crafting():
+    while have_enough_khoang_thach(10):
+        while utils.get_syncId() <= 0:
+            time.sleep(1)
+            continue
+        utils.click_on_menu(19)
+        time.sleep(15)
+
+def main():
+    # farming()
+    crafting()
 
 main()
