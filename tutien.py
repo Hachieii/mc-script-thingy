c631642ead305bfdbe7839e2be70f7b1fb7bb12f
Hachieii <hachieii07@gmail.com>
2026-04-20 12:44:22 +0700

    full automation for going to farm land

--- tutien.py
+++ tutien.py
@@ -1,8 +1,9 @@
 import minescript
 import time
 import utils
+from math import sqrt
 
-def main():
+def go_to_farm_land():
     minescript.player_press_sprint(True)
     minescript.player_press_forward(True)
     minescript.player_press_jump(True)
@@ -15,7 +16,24 @@ def main():
     minescript.player_press_jump(False)
     
     time.sleep(0.2)
-    
     utils.click_on_menu(11)
+    time.sleep(0.2)
+    
+def is_dead(x, y, z):
+    cx, cy, cz = minescript.player_position()
+    delta = sqrt((x - cx)*(x - cx) + (y - cy)*(y - cy) + (z - cz)*(z - cz))
+    return delta > 1.5
+
+def main():
+    while 1:
+        go_to_farm_land()
+        
+        # should be tp to 1000.505 60 0.416
+        x, y, z = minescript.player_position()
+        
+        while not is_dead(x, y, z):
+            time.sleep(1)
+            continue
+    
 
 main()
