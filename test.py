f8b8c89fb4820f60ef4c9df905d57dab5edb23ff
Hachieii <hachieii07@gmail.com>
2026-04-20 17:12:19 +0700

    add option to view right tab value

--- test.py
+++ test.py
@@ -1,6 +1,9 @@
 import minescript
 import win32api
 import time
+import utils
+import pyperclip
+import re
 
 def test():
     # while 1:
@@ -11,10 +14,22 @@ def test():
     #     minescript.echo(minescript.player_get_targeted_block().type)
     #     time.sleep(1)
     
-    while 1:
-        minescript.echo(win32api.GetCursorPos())
-        time.sleep(1)
+    # while 1:
+    #     minescript.echo(win32api.GetCursorPos())
+    #     time.sleep(1)
     
+    raw = utils.get_scoreboard_info()
+    # pyperclip.copy(raw)
+    minescript.echo(raw)
+    
+    minescript.echo('done')
+    # res = re.search(r'(\d+)\s*\xca\x9f\xc9\xaa\xc9\xb4\xca\x9c', raw)
+    
+    # minescript.echo('\xca\x9f\xc9\xaa\xc9\xb4\xca\x9c' in raw)
+    # for v in raw:
+    #     minescript.echo(type(v))
+    
+    # minescript.echo(utils.dump())
     return
 
 test()
--- utils.py
+++ utils.py
@@ -43,7 +43,77 @@ def click_on_menu(slotId, button=0, actionType="PICKUP"):
         actionType,
         player
     )
+
+def get_scoreboard_info():
+    world = instance.field_1687
+    scoreboard = world.method_8428() # getScoreboard()
+    teams = scoreboard.method_1159() # getTeams()
+    
+    patterns = {
+        "linh_thach": "\\u029f\\u026a\\u0274\\u029c \\u1d1b\\u029c\\u1ea1\\u1d04\\u029c",
+        "khoang_thach": "\\u1d0b\\u029c\\u1d0f\\u00e1\\u0274\\u0262 \\u1d1b\\u029c\\u1ea1\\u1d04\\u029c",
+        "thao_duoc": "\\u1d1b\\u029c\\u1ea3\\u1d0f \\u1d05\\u01b0\\u1ee3\\u1d04",
+        "thien_xu": "\\u1d1b\\u029c\\u026a\\u00ea\\u0274 x\\u1d1c",
+    }
+    tu_vi_pattern = "\\u1d1b\\u1d1c \\u1d20\\u026a" # special case
+    
+    res = {
+        "tu_vi": "",
+        "linh_thach": "",
+        "khoang_thach": "",
+        "thao_duoc": "",
+        "thien_xu": ""
+    }
+    
+    for team in teams.toArray():
+        # prefix = str(team.method_1144()) # getPrefix()
+        # suffix = str(team.method_1136()) # getSuffix()
+        # full_text = prefix + suffix
+        
+        # It is known that the information we are looking for only appear in prefix
+        
+        content = str(team.method_1144())
+        
+        for key, pattern in patterns.items():
+            if len(res[key]):
+                continue
+            
+            r = content.find(pattern)
+            if r == -1:
+                continue
+            
+            l = r - 2
+            while content[l] != ' ':
+                l -= 1
+            
+            res[key] = content[l + 1: r - 1]
+       
+        if len(res["tu_vi"]):
+            continue
+            
+        idx = content.find(tu_vi_pattern)
+        if idx == -1:
+            continue
+            
+        idx = content.find("literal", idx)
+        l = idx + len("literal{")
+        r = content.find("}", l)
+        
+        v1 = content[l:r]
+        
+        tmp = content.find("literal{/}", idx) + len("literal{/}")
+        idx = content.find("literal", tmp)
+        
+        l = idx + len("literal{")
+        r = content.find(' ', l)
+        
+        v2 = content[l:r]
+        
+        res["tu_vi"] = f"{v1}/{v2}"
+    
+    return res
 """)
 
 get_syncId = pyjinn.get("get_syncId")
 click_on_menu = pyjinn.get("click_on_menu")
+get_scoreboard_info = pyjinn.get("get_scoreboard_info")
