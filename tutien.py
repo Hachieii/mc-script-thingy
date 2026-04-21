6999f964df0e0f729a282a48edc69cd8dd29e8d3
Hachieii <hachieii07@gmail.com>
2026-04-21 07:04:08 +0700

    fixed some bugs and improve performance

--- tutien.py
+++ tutien.py
@@ -66,39 +66,22 @@ def have_enough_khoang_thach(ammount):
     cur = price_parser(data["khoang_thach"])
     return cur >= ammount
 
-def get_current_progess():
+def can_upgrade():
     content = str(utils.get_menu_info()[33])
-    match = re.search(r'\((\d+)/(\d+)\)', content)
+    progress = re.search(r'\((\d+)/(\d+)\)', content)
+    wait_time = re.search(r'literal\{(\d+:\d+:\d+)\}', content)
     
-    a = int(match.group(1))
-    b = int(match.group(2))
-    
-    return [a, b]
-
-def have_wait_time():
-    content = str(utils.get_menu_info()[33])
-    match = re.search(r'literal\{(\d+:\d+:\d+)\}', content)
-    return not match
+    return int(progress.group(1)) >= int(progress.group(2)) and (not wait_time)
         
 def crafting():
     while have_enough_khoang_thach(10):
         while utils.get_syncId() <= 0:
-            time.sleep(1)
+            time.sleep(0.5)
             continue
         
-        a, b = get_current_progess()
-        if (not have_wait_time()) and a >= b:
+        if can_upgrade():
             utils.click_on_menu(33) # assuming have enough linh thach
             time.sleep(0.5)
-            
-            # reopen the menu
-            # minescript.player_press_backward(True)
-            # time.sleep(0.25)
-            # minescript.player_press_backward(False)
-            
-            # minescript.player_press_forward(True)
-            # time.sleep(0.5)
-            # minescript.player_press_forward(False)
             
         utils.click_on_menu(19)
         time.sleep(15)
